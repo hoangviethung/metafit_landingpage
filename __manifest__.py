83da9614a6af90568a0ceb0fe0568eaec60cb950
hoangviethung <hoangviet.hung.009@gmail.com>
2023-04-19 11:43:08 +0700

    update ui

--- __manifest__.py
+++ __manifest__.py
@@ -22,11 +22,23 @@
     'application': True,
     'auto_install': False,
     'assets': {
-            'web.assets_frontend': [
-                    '/metafit_landingpage/static/src/css/style.css',
-                    '/metafit_landingpage/static/src/libs/swiper/swiper-bundle.min.css',
-                    '/metafit_landingpage/static/src/libs/swiper/swiper-bundle.min.js',
-                    '/metafit_landingpage/static/src/js/index.js',
-                ],
-            },
+        'web.assets_frontend':[
+            '/metafit_landingpage/static/src/scss/mixins.scss',
+            '/metafit_landingpage/static/src/scss/common.scss',
+            '/metafit_landingpage/static/src/scss/fonts.scss',
+            '/metafit_landingpage/static/src/scss/normalize.scss',
+            '/metafit_landingpage/static/src/scss/layouts/navbar.scss',
+            '/metafit_landingpage/static/src/scss/layouts/footer.scss',
+            '/metafit_landingpage/static/src/scss/pages/homepage.scss',
+            '/metafit_landingpage/static/src/libs/swiper/swiper-bundle.min.css',
+            '/metafit_landingpage/static/src/libs/swiper/swiper-bundle.min.js',
+            '/metafit_landingpage/static/src/js/index.js',
+        ],
+        'web._assets_primary_variables': [
+            '/metafit_landingpage/static/src/scss/primary_variables.scss'
+        ],
+        'web._assets_frontend_helpers': [
+            ('prepend', '/metafit_landingpage/static/src/scss/user_custom_bootstrap_overridden.scss'),
+        ],
+    },
 }
